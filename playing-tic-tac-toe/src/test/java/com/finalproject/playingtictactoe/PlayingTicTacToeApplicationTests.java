fc34fbb8db6d2b64fcf0a11b2b7c175ee5e88166
Pranshu Dixit <dixit.pr@northeastern.edu>
2022-04-25 13:50:09 -0400

    Seeding project

---
+++ playing-tic-tac-toe/src/test/java/com/finalproject/playingtictactoe/PlayingTicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.playingtictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayingTicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
